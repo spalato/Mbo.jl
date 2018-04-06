d9b9770d8f2b2e1ad578f104a53ebfd13d035c2d
S Palato <samuel.palato@mail.mcgill.ca>
2018-04-06 13:13:07 -0400

    Restored comments in core

--- src/core.jl
+++ src/core.jl
@@ -1,4 +1,3 @@
-# core API
 
 # unpack arguments
 # unpack 'system' arguments
@@ -9,7 +8,16 @@ function R1(g::TimeGrid{<:Real,3}, s::System, p::HilbertPath{5})
     )
 end
 
-# try another approach
-R1(t1, t2, t3, ws::NTuple{3,Real}, ls::NTuple{6,Function}) = R1(t1, t2, t3, ws..., ls...)
-R1(g::TimeGrid{<:Real,3}, ws, ls) = R1.(grid(g)..., ws, ls)
+# try another approach ... this doesn't work: it tries to broadcast the tuple too.
+# rely on '@eval'?
+R1(t1, t2, t3, ws::NTuple{3,Real}, ls::NTuple{6,Function}) = R1(t1, t2, t, ws..., ls...)
+R1(g::TimeGrid{<:Real,3}, ws, ls) = R1.(grid(g), ws, ls)
+#=
+R1(g::TimeGrid{T,3}, ws::NTuple{3,Real}, ls::NTuple{6,Function}) where {T} = R1(grid(g), ws, ls)
+R1(g::TimeGrid{T,3}, s::System) where {T} = sum(p->R1(g, s, p), hilbert_paths(s, 3))
 
+# Unpack timegrid arguments
+R1(t::NTuple{3,AbstractArray}, w::NTuple{3,Real}, g::NTuple{6,Function}) = 
+    R1(t, w..., g...)
+R1(t::NTuple{3,AbstractArray}, v...) = R1.(t[1], t[2], t[3], v...)
+=#
