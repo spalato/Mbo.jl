6a41c27723b2a5fe248ce553c77c81a26a534a09
spalato <samuel.palato@mail.mcgill.ca>
2020-02-10 11:00:37 +0100

    Update run_cfg.ps1

--- examples/aimd_full/run_cfg.ps1
+++ examples/aimd_full/run_cfg.ps1
@@ -3,11 +3,11 @@ if (!$cfg) {
     $cfg=$(ls *.yaml)
 }
 foreach ($c in $cfg) {
-    $root=$(Select-String -Path $cfg -Pattern '^rootname:\W*(.*)$').Matches.Groups[1].value
-    julia .\linear_xcf.jl $cfg
-    python ..\plot_linear.py $cfg
-    julia .\third_order_xcf.jl $cfg
-    ls ${root}_r[nr].bin | %{julia .\spec_ft.jl $cfg $_ $_.Name.Replace("_r", "_s")}
-    julia .\spec_abs.jl $cfg $root ${root}_sa.bin
-    python ..\plot_2d.py $cfg
+    $root=$(Select-String -Path $c -Pattern '^rootname:\W*(.*)$').Matches.Groups[1].value
+    julia .\linear_xcf.jl $c
+    python ..\plot_linear.py $c
+    julia .\third_order_xcf.jl $c
+    ls ${root}_r[nr].bin | %{julia .\spec_ft.jl $c $_ $_.Name.Replace("_r", "_s")}
+    julia .\spec_abs.jl $c $root ${root}_sa.bin
+    python ..\plot_2d.py $c
 }
