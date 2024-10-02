310dabf93e095a6f031fbd0593e2bf805ff3bfa3
SPalato <7659022+spalato@users.noreply.github.com>
2024-10-02 21:06:27 +0200

    Compared numerical results.
    Compared calculation results from the current version to reference results obtained using julia v0.6. New version is as right (or wrong) as the previous one.
    
    Specifically, the rephasing and non-rephasing arrays were compared elementwise.
    Largest difference is 5.9E-14, out of unit quantities. Test passed.

--- examples/compare_bin.jl
+++ examples/compare_bin.jl
@@ -1,22 +1,34 @@
 import YAML
 using Mbo
+using Printf
 
 function run(args)
 cfgf = args[1]
+@debug "Opening: " cfgf
 dir = dirname(cfgf)
 cfg = open(YAML.load, cfgf)
 rootname = cfg["rootname"]
-t1_n = cfg["t1_n"]
-t2_n = cfg["t2_n"]
-t3_n = cfg["t3_n"]
-t1_max = cfg["t1_max"]
-t2_max = cfg["t2_max"]
-t3_max = cfg["t3_max"]
-tg = TimeGrid(
-    range(0, t1_max, t1_n),
-    range(0, t2_max, t2_n),
-    range(0, t3_max, t3_n),
-)
+try
+    t1_n = cfg["t1_n"]
+    t2_n = cfg["t2_n"]
+    t3_n = cfg["t3_n"]
+    t1_max = cfg["t1_max"]
+    t2_max = cfg["t2_max"]
+    t3_max = cfg["t3_max"]
+    global tg = TimeGrid(
+        range(0, t1_max, t1_n),
+        range(0, t2_max, t2_n),
+        range(0, t3_max, t3_n),
+    )
+catch err
+    if isa(err, KeyError)
+        t1 = convert.(Float64, cfg["t1"])
+        t2 = convert.(Float64, cfg["t2"])
+        t3 = convert.(Float64, cfg["t3"])
+        global tg = TimeGrid(t1, t2, t3)
+    else rethrow()
+    end
+end
 
 for sig in ["rr", "rn"]
     data = Array{ComplexF64}(undef, size(tg))
@@ -26,9 +38,9 @@ for sig in ["rr", "rn"]
     read!(joinpath(dir, "$(rootname)_$(sig).bin.ref06"), ref)
 
     if all(isapprox.(data, ref))
-        @info "$(cfgf) $(sig) ok       abs:$(maximum(abs.(data-ref)))"  
+        @info "$(cfgf) $(sig) ok       abs="*@sprintf("%.06e", maximum(abs.(data-ref)))
     else
-        @info "$(cfgf) $(sig) DIFF!!!  abs:$(maximum(abs.(data-ref)))"
+        @info "$(cfgf) $(sig) DIFF!!!  abs="*@sprintf("%.06e", maximum(abs.(data-ref)))
     end
 end
 
